f89563cd2ef0413a022bc58bcc052e625190315b
MGV <michaeldoestech@gmail.com>
2018-01-09 20:12:20 -0500

    "[Release] Automated builder release"

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,33 +8,56 @@ const gulp = require('gulp')
 const bump = require('gulp-bump')
 const log = require('gulplog')
 const git = require('gulp-git')
-const { spawn } = require('child_process')
+const spawn = require('child_process').spawn;
+const cwd = process.cwd()
 
 gulp.task('builder-bump-version', function () {
     return gulp.src(['./package.json'])
                 .pipe(bump({ type: "patch" }).on('error', log.error))
-                .pipe(gulp.dest('/'))
+                .pipe(gulp.dest('.'))
 })
 
 gulp.task('builder-commit-changes', function () {
     return gulp.src('.')
-                .pipe(git.commit('[Builder] Pre-release', { args: '-m'}))
-})
-
-gulp.task('builder-add-files', function () {
-    return gulp.src('.')
                 .pipe(
-                    git.add()
+                    spawn({
+                        cmd: "git",
+                        args: [
+                            "commit",
+                            "-m",
+                            "'[Release] Automated release'"
+                        ]
+                    })
                 )
 })
 
+gulp.task('builder-add-files', function () {
+    return spawn('git', ['add', '-u'], { cwd: cwd, stdio: 'inherit' })
+})
+
 gulp.task('builder-push-master', function () {
-    git.push('origin', ['master'], {}, function (err) {
-        if(err) throw err
+    spawn({
+        cmd: "git",
+        args: [
+            'push',
+            '-u',
+            'origin',
+            'master'
+        ]
     })
 })
 
 /** Pre-release tasks */
 
-gulp.task('pre-release-builder', ['builder-add-files', 'builder-commit-changes', 'builder-push-master'], function () {
+gulp.task('pre-release-builder', function () {
+    spawn('git', ['add', '*'], { cwd: cwd, stdio: 'inherit' })
+    spawn('git', ['commit', '-m', '"[Release] Automated builder release"'], { cwd: cwd, stdio: 'inherit'})
+    spawn('git', ['push', '-u', 'origin', 'master'], { cwd: cwd, stdio: 'inherit'})
+})
+
+/** Default */
+
+gulp.task('default', function (done) {
+    spawn('npm', ['install'], { cwd: cwd, stdio: 'inherit' })
+        .on('close', done)
 })
